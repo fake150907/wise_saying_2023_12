2d761e5112e68ff70a5a24b3440abaedf2891a36
fake150907 <yungayeon223@gmail.com>
2023-12-21 10:29:37 +0900

    리팩토링, SystemController, WiseSayingController로 기능 분리

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -1,10 +1,9 @@
 package com.ws;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-import com.ws.wiseSaying.entity.WiseSaying;
+import com.ws.system.controller.SystemController;
+import com.ws.wiseSaying.controller.WiseSayingController;
 
 public class App {
 
@@ -15,42 +14,20 @@ public class App {
 	}
 
 	public void run() {
-
 		System.out.println("== 명언 앱 실행 ==");
-
-		List<WiseSaying> wiseSayings = new ArrayList<>();
-		int lastId = 0;
+		SystemController systemController = new SystemController();
+		WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
 		while (true) {
 			System.out.print("명령어) ");
 			String cmd = sc.nextLine().trim();
-
 			if (cmd.equals("종료")) {
-				System.out.println("== 명언 앱 종료 ==");
+				systemController.exit();
 				break;
 			} else if (cmd.equals("등록")) {
-				int id = lastId + 1;
-				System.out.print("명언 : ");
-				String content = sc.nextLine().trim();
-				System.out.print("작가 : ");
-				String author = sc.nextLine().trim();
-				wiseSayings.add(new WiseSaying(id, content, author));
-
-				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-				lastId++;
+				wiseSayingController.write();
 			} else if (cmd.equals("목록")) {
-				if (wiseSayings.size() == 0) {
-					System.out.println("등록 된 명언이 없습니다.");
-				} else {
-					System.out.println("==========================");
-					System.out.println("번호  |  작가  |  명언");
-					System.out.println("==========================");
-					for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-						WiseSaying ws = wiseSayings.get(i);// 코드가 길어지면 길어질수록 변수에 담아서 가독성이 더 좋게끔 짧게 코딩해줘야한다.
-						System.out.printf("%d | %s | %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
-						
-					}
-				}
+				wiseSayingController.list();
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -1,5 +1,48 @@
 package com.ws.wiseSaying.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.ws.wiseSaying.entity.WiseSaying;
+
 public class WiseSayingController {
+	
+	private Scanner sc;
+	private int lastId;
+	private List<WiseSaying> wiseSayings;
+
+	public WiseSayingController(Scanner sc) {
+		this.sc = sc;
+		lastId = 0;
+		wiseSayings = new ArrayList<>();
+	}
+
+	public void write() {
+		int id = lastId + 1;
+		System.out.print("명언 : ");
+		String content = sc.nextLine().trim();
+		System.out.print("작가 : ");
+		String author = sc.nextLine().trim();
+		wiseSayings.add(new WiseSaying(id, content, author));
+
+		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+		lastId++;
+	}
+
+	public void list() {
+		if (wiseSayings.size() == 0) {
+			System.out.println("등록 된 명언이 없습니다.");
+		} else {
+			System.out.println("==========================");
+			System.out.println("번호  |  작가  |  명언");
+			System.out.println("==========================");
+			for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+				WiseSaying ws = wiseSayings.get(i);// 코드가 길어지면 길어질수록 변수에 담아서 가독성이 더 좋게끔 짧게 코딩해줘야한다.
+				System.out.printf("%d | %s | %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
+
+			}
+		}
+	}
 
 }
