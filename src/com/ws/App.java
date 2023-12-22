08207a8da1682a02aa7a19d14ddba4c578126095
fake150907 <yungayeon223@gmail.com>
2023-12-22 17:18:30 +0900

    주석 : 내용 설명

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -3,39 +3,39 @@ package com.ws;
 import com.ws.system.controller.SystemController;
 import com.ws.wiseSaying.controller.WiseSayingController;
 
-public class App {
-	private byte sysytem_status = 1;
+public class App { // 명령이 들어오면 어디쪽으로가서 일 처리해~ 를 담당할 App클래스
+	private byte sysytem_status = 1; // run안쪽 while문의 조건의 상태를 결정짓는 변수. 
 
-	public void run() {
+	public void run() { // 명언 앱을 실행시키기 위한 메서드
 		System.out.println("== 명언 앱 실행 ==");
 
-		SystemController systemController = new SystemController();
-		WiseSayingController wiseSayingController = new WiseSayingController();
+		SystemController systemController = new SystemController();// systemController한테 SystemController객체를 연결시켜주면서 SystemController의 일을 맡기려고 만든 변수.
+		WiseSayingController wiseSayingController = new WiseSayingController();// wiseSayingController한테 WiseSayingController객체를 연결시켜주면서 WiseSayingController의 일을 맡기려고 만든 변수.
 
-		while (sysytem_status == 1) {
-			System.out.print("명령어) ");
-			String cmd = Container.getScanner().nextLine().trim();
-			Rq rq = new Rq(cmd);
+		while (sysytem_status == 1) {// 무한반복을 통해서 앱을 실행해야해서 안쪽 조건문이 참일 때 무한반복
+			System.out.print("명령어) ");// 여기 명령어 입력해주세요의 명령어)
+			String cmd = Container.getScanner().nextLine().trim();// cmd라는 변수안에 Container를 통해서 Scanner실행하고 nextLine통해서 명령어 입력받고 입력받은 명령어의 좌우 공백을 trim이 제거해준다.
+			Rq rq = new Rq(cmd); // Rq타입의 rq를 Rq객체를 연결시키면서 cmd를 인자값으로 넘겨주면서 기능에 맞게 일처리를 해달라고 부탁한다.
 			
-			switch(rq.getActionCode()) {
-			case "종료":
-				systemController.exit();
-				sysytem_status = 0;
-				break;
-			case "등록":
-				wiseSayingController.write();
-				break;
-			case "목록":
-				wiseSayingController.list();
-				break;
-			case "삭제":
-				wiseSayingController.remove(rq);					
-				break;
-			case "수정":
-				wiseSayingController.modify(rq);
-				break;
-			default:
-				System.out.println("존재하지 않는 명령어입니다.");
+			switch(rq.getActionCode()) { // rq에 연결되어있는 Rq객체로 getActionCode메서드를 실행해서 어떤 일을 할지 라우터해준다.
+			case "종료": // rq.getActionCode()통해서 얻은 값이 종료라면 아래 코드를실행해라.
+				systemController.exit(); // systemController한테 exit메서드를 실행해달라고 요청함.
+				sysytem_status = 0; // 그다음 while문의 조건이 false가 되게 만들어서 종료시키기 위해서 sysytem_status의 값을 0으로 재할당한다.
+				break;// switch문을 빠져나감.
+			case "등록":// rq.getActionCode()통해서 얻은 값이 등록이라면 아래 코드를실행해라.
+				wiseSayingController.write(); // wiseSayingController한테 write()메서드를 실행해달라고 요청함.
+				break;// switch문을 빠져나감.
+			case "목록":// rq.getActionCode()통해서 얻은 값이 목록이라면 아래 코드를실행해라.
+				wiseSayingController.list();// wiseSayingController한테 list()메서드를 실행해달라고 요청함.
+				break;// switch문을 빠져나감.
+			case "삭제":// rq.getActionCode()통해서 얻은 값이 삭제라면 아래 코드를실행해라.
+				wiseSayingController.remove(rq);// wiseSayingController한테 remove(rq)메서드를 실행해달라고 요청함.			
+				break;// switch문을 빠져나감.
+			case "수정":// rq.getActionCode()통해서 얻은 값이 수정이라면 아래 코드를실행해라.
+				wiseSayingController.modify(rq);// wiseSayingController한테 modify(rq)메서드를 실행해달라고 요청함.
+				break;// switch문을 빠져나감.
+			default:// 위의 case에 해당하지 않는 명령어라면 아래를 출력함.
+				System.out.println("존재하지 않는 명령어입니다.");// 존재하지 않는 명령어라는걸 알려주기 위해 안의 내용을 출력함.
 			}	
 		}
 	}
--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -7,73 +7,72 @@ import com.ws.Rq;
 import com.ws.wiseSaying.entity.WiseSaying;
 import com.ws.wiseSaying.service.WiseSayingService;
 
-public class WiseSayingController {
+public class WiseSayingController {// App한테 받은 명령을 수행하려고 만든 클래스
 
-	private WiseSayingService wiseSayingService;
+	private WiseSayingService wiseSayingService;// WiseSayingService타입의 wiseSayingService변수.
 
-	public WiseSayingController() {
-		wiseSayingService = new WiseSayingService();
+	public WiseSayingController() { // 변수에 객체연결이나 값을 넣어주려고 생성자 메서드를 이용한다.
+		wiseSayingService = new WiseSayingService();// 명령어에 따른 요청을 처리하기 위해 wiseSayingService변수에 WiseSayingService객체를 연결.
 	}
 
-	public void write() {
-		System.out.print("명언 : ");
-		String content = Container.getScanner().nextLine().trim();
-		System.out.print("작가 : ");
-		String author = Container.getScanner().nextLine().trim();
+	public void write() { // wiseSayingController가 write()메서드를 실행하려고 만든 메서드
+		System.out.print("명언 : ");// 등록해야할 명언을 입력받으려고 명언 : 을 줄바꿈 없이 출력한다.
+		String content = Container.getScanner().nextLine().trim();// content안에 Container를 통해서 Scanner를 가져와서 nextLine()통해서 어떤 명언을 입력받을지 적고 trim()을 통해서 좌우공백 제거를 해준다.
+		System.out.print("작가 : ");// 등록해야할 작가를 입력받으려고 작가 : 을 줄바꿈 없이 출력한다.
+		String author = Container.getScanner().nextLine().trim();// author안에 Container를 통해서 Scanner를 가져와서 nextLine()통해서 어떤 작가를 입력받을지 적고 trim()을 통해서 좌우공백 제거를 해준다.
 
-		int id = wiseSayingService.write(content, author);
+		int id = wiseSayingService.write(content, author);// 오직 정수만을 담을 수 있는 id라는 공간안에 wiseSayingService를 통해서 write(content, author)메서드를 실행시키고 return받은 int값을 저장한다.
 
-		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+		System.out.printf("%d번 명언이 등록되었습니다.\n", id);// printf출력문의 서식지정자의 값에 위의 저장된 id값을 통해서 몇번 명언이 등록되었다는걸 알려주기위한 출력문이다.
 	}
 
-	public void list() {
-		List<WiseSaying> wiseSayings = wiseSayingService.findAll();
+	public void list() { // wiseSayingController가 list()메서드를 실행하려고 만든 메서드
+		List<WiseSaying> wiseSayings = wiseSayingService.findAll();// WiseSaying타입만 담을 수 있는 List를 연결할 wiseSayings라는 공간안에 wiseSayingService한테 findAll() 메서드를 실행시켜서 return받은 List<WiseSaying>타입의 List를 담겠다.
 
-		System.out.println("번호  /  작가  /  명언  ");
-		System.out.println("=====================");
-		for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-			WiseSaying ws = wiseSayings.get(i);
-			System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
-
+		System.out.println("번호  /  작가  /  명언  ");// 목록화를 어떤 형식으로 해줄지 출력한다.
+		System.out.println("=====================");// 목록화를 어떤 형식으로 해줄지 출력한다.
+		for (int i = wiseSayings.size() - 1; i >= 0; i--) { // wiseSayings사이즈에 따라서 제일 최신글부터 보여주려고 만든 초기값, 종료조건,스텝이다.
+			WiseSaying ws = wiseSayings.get(i);// 오직 WiseSaying타입만 담을 수 있는 ws라는 공간안에 wiseSayings의 list를 i값의 변화에 따라서 get함수를 이용해서 담을 것이다.
+			System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent()); // 그리고 wiseSayings에 어떤 list가 담겨있는지 출력해준다.
 		}
 
-		if (wiseSayings.size() == 0) {
-			System.out.println("등록 된 명언이 없습니다.");
+		if (wiseSayings.size() == 0) {// 만약 wiseSayings의 사이즈가 0이라면 아래 실행문을 출력하겠다.
+			System.out.println("등록 된 명언이 없습니다.");// 등록 된 명언이 없을시에 이 출력문을 출력함.
 		}
 	}
 
-	public void remove(Rq rq) {
+	public void remove(Rq rq) {// wiseSayingController가 인자값으로 Rq타입의 rq를 받는 remove()메서드를 실행하려고 만든 메서드
 
-		int id = rq.getIntParam("id", -1);
+		int id = rq.getIntParam("id", -1);// 오직 int타입만을 담을 수 있는 id라는 공간안에 매개변수를 통해 받은 인자 rq를 통해서 getIntParam("id", -1)실행시키고 return받은 값을 담겠다.
 
-		if (id == -1) {
-			System.out.println("id(정수)를 제대로 입력해주세요");
-			return;
+		if (id == -1) {// id의 값이 -1이라면 if문 안쪽을 실행할것이다. // 만약 rq에 연결된 rq객체로 넘어온 cmd라는 인자가 기존에 갖춰진 형식에 맞지 않게 입력을 했다면 -1을 리턴한다.
+			System.out.println("id(정수)를 제대로 입력해주세요");// id(정수)를 제대로 입력해주세요 출력해준다.
+			return; // return;해주면서 void 메서드가 끝나게해준다.
 		}
-		WiseSaying wiseSaying = wiseSayingService.findById(id);
+		WiseSaying wiseSaying = wiseSayingService.findById(id);// 오직 WiseSaying만을 담을 수 있는 wiseSaying공간에 wiseSayingService로 findById(id) 메서드를 실행시키고 return된 값을 담겠다.
 
-		if (wiseSaying == null) {
-			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
-			return;
+		if (wiseSaying == null) { // 만약 wiseSaying가 null이라면 if문 안쪽을 실행할 것이다. 
+			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id); // printf출력문의 서식지정자의 값에 위의 저장된 id값을 통해서 몇번 명언은 존재하지 않다는걸 알려주기위한 출력문이다.
+			return;// return;를 통해 void메서드를 종료시킨다.
 		}
-		wiseSayingService.remove(wiseSaying);
-		System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+		wiseSayingService.remove(wiseSaying);// 위의 if문들을 다 통과해서 여기까지 왔다면 wiseSayingService로 object기반으로 remove(wiseSaying)메서드를 실행시킨다.
+		System.out.printf("%d번 명언이 삭제되었습니다.\n", id);// 위의 메서드가 다 끝나면 printf출력문의 서식지정자의 값에 위의 저장된 id값을 통해서 몇번 명언이 삭제 되었는지 알려주는 출력문이다.
 	}
 
-	public void modify(Rq rq) {
+	public void modify(Rq rq) {// wiseSayingController가 인자값으로 Rq타입의 rq를 받는 modify()메서드를 실행하려고 만든 메서드
 
-		int id = rq.getIntParam("id", -1);
+		int id = rq.getIntParam("id", -1);// // 오직 int타입만을 담을 수 있는 id라는 공간안에 매개변수를 통해 받은 인자 rq를 통해서 getIntParam("id", -1)실행시키고 return받은 값을 담겠다.
 
-		if (id == -1) {
-			System.out.println("id(정수)를 제대로 입력해주세요");
-			return;
+		if (id == -1) {// id의 값이 -1이라면 if문 안쪽을 실행할것이다.
+			System.out.println("id(정수)를 제대로 입력해주세요");// id(정수)를 제대로 입력해주세요 출력해준다.
+			return;// return;해주면서 void 메서드가 끝나게해준다.
 		}
 
-		WiseSaying wiseSaying = wiseSayingService.findById(id);
+		WiseSaying wiseSaying = wiseSayingService.findById(id);// 오직 WiseSaying만을 담을 수 있는 wiseSaying공간에 wiseSayingService로 findById(id) 메서드를 실행시키고 return된 값을 담겠다.
 
-		if (wiseSaying == null) {
-			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
-			return;
+		if (wiseSaying == null) {// 만약 wiseSaying가 null이라면 if문 안쪽을 실행할 것이다. 
+			System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);// printf출력문의 서식지정자의 값에 위의 저장된 id값을 통해서 몇번 명언은 존재하지 않다는걸 알려주기위한 출력문이다.
+			return;// return;해주면서 void 메서드가 끝나게해준다.
 		}
 
 		System.out.println("(기존)명언 : " + wiseSaying.getContent());
